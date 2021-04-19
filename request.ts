1c9b3a8860c1729aa787527dfcf3fc111139d3a4
Reynier de la Rosa <rdelarosa@ovebanco.com>
2021-04-19 11:04:12 +0200

    fix request url for benchmark

--- request.ts
+++ request.ts
@@ -1,29 +1,29 @@
 import request from "request";
-import { HTTP_BASE_URL, HTTPS_BASE_URL } from "./_constants";
+import { HTTP_BASE_URL, HTTPS_BASE_URL, PATH } from "./_constants";
 import { IBenchmarkModel } from "./types/benchmark-models";
 
 const benchmarkModels: IBenchmarkModel[] = [
   {
     fn: (defer: any) => {
-      request.get(HTTP_BASE_URL, () => defer.resolve());
+      request.get(HTTP_BASE_URL + PATH, () => defer.resolve());
     },
     target: "[request] http [GET]",
   },
   {
     fn: (defer: any) => {
-      request.post(HTTP_BASE_URL, () => defer.resolve());
+      request.post(HTTP_BASE_URL + PATH, () => defer.resolve());
     },
     target: "[request] http [POST]",
   },
   {
     fn: (defer: any) => {
-      request.get(HTTPS_BASE_URL, () => defer.resolve());
+      request.get(HTTPS_BASE_URL + PATH, () => defer.resolve());
     },
     target: "[request] https [GET]",
   },
   {
     fn: (defer: any) => {
-      request.post(HTTPS_BASE_URL, () => defer.resolve());
+      request.post(HTTPS_BASE_URL + PATH, () => defer.resolve());
     },
     target: "[request] https [POST]",
   },
